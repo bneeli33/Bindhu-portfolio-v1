d8b6b5b82c46fc1fde3e7812a9db8a060a04ad1f
bindhu-sym <bindhu@symbols.app>
2025-10-31 12:17:36 -0500

    Initial commit

---
+++ lib/data.ts
@@ -0,0 +1,232 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import cryptoCrowdImg from "@/public/cryptocrowd2.png";
+import ozdevsImg from "@/public/ozdevs-two.png";
+import assetTradingImg from "@/public/asset-trading.jpg";
+import euphorusImg from "@/public/euphorus-two.png";
+import euphorusBackendImg from "@/public/euphorus-backend.png";
+import techprowlImg from "@/public/techprowl-two.png";
+import taskManagerImg from "@/public/task-manager.png";
+import familyTreeImg from "@/public/bfsproject.jpg";
+import binaryGameImg from "@/public//binary-game.png";
+import humanforceLogo from "@/public/humanforceLogo.png";
+import uqLogo from "@/public/uqLogo.png";
+import youpayLogo from "@/public/youpayLogo.png";
+import m3Logo from "@/public/m3Logo.jpg";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Education",
+    hash: "#education",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Software Engineer",
+    location: "Humanforce",
+    description:
+      "Software engineer in the Humanforce platform team. We are a feature enablement team dedicated to developing features for developers, ensuring they are easier to create. We also look after issues of scale and technical debt for developers and the platform.",
+    icon: React.createElement(CgWorkAlt),
+    date: "January 2024 - Present",
+    logo: humanforceLogo
+  },
+  {
+    title: "Software Engineer",
+    location: "The University of Queensland",
+    description:
+      "Full-stack software engineer in the Application Development and Support team. Worked on web applications such as CAHP (Casual Academic Hire and Payment), UQ Maps, UQ Donations, and Programs and Courses.",
+    icon: React.createElement(CgWorkAlt),
+    date: "November 2022 - January 2024",
+    logo: uqLogo
+  },
+  {
+    title: "Developer",
+    location: "YouPay",
+    description:
+      "Developed new features using Laravel, Vue.js, PHP, JavaScript, Tailwind CSS, MySQL and REST APIs. Created an email notification system for mechants to receive weekly/monthly summaries of YouPay carts created, paid and cancelled. I also integrated YouPay into dozens of stores.",
+    icon: React.createElement(CgWorkAlt),
+    date: "February 2022 - November 2022",
+    logo: youpayLogo
+  },
+  {
+    title: "Web Developer",
+    location: "M3 Digital - Contract",
+    description:
+      "Front-end development for a variety of Shopify stores such as FitazFK, Rider Collective, Masseuse Massage, Francesca Jewellery and Salty Captain. Developed high quality landing pages and front end features using HTML, CSS, SCSS, JavaScript and Liquid.",
+    icon: React.createElement(CgWorkAlt),
+    date: "September 2021 - February 2022",
+    logo: m3Logo
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Developer Job Matching Portal",
+    description:
+      "Reverse Job board for companies looking to hire developers in Australia. Filter by experience, location and keyboards.  Companies can pay a monthly fee to be able to message developers on the site and view their information.",
+    tags: ["PHP", "Laravel", "JavaScript", "Tailwind", "MySQL"],
+    icons: [
+      "logos:laravel",
+      "logos:php",
+      "logos:javascript",
+      "devicon:tailwindcss",
+      "logos:mysql",
+    ],
+    imageUrl: ozdevsImg,
+    githubLink: "https://github.com/ben04rogers/ozdevs-v2",
+  },
+  {
+    title: "Crypto Sentiment Analysis",
+    description:
+      "Cryptocurrency sentiment analysis website based on Twitter posts. Users can see the overall sentiment (positive, negative or neutral) of different Cryptocurrencies based on what people are currently posting on Twitter. Users can also see keywords and an average sentiment score. Stateless application that uses Elasticache and S3 on AWS. It also used EC2 and auto-scaling to manage demand.",
+    tags: ["JavaScript", "React", "Express.js", "AWS"],
+    icons: [
+      "logos:aws",
+      "logos:javascript",
+      "logos:react",
+      "logos:bootstrap",
+      "logos:express",
+    ],
+    imageUrl: cryptoCrowdImg,
+    githubLink: "https://github.com/ben04rogers/cab432-assignment-2",
+    demoLink: "https://www.youtube.com/watch?v=uhyC0M4WIl4",
+  },
+  {
+    title: "Company Asset Trading",
+    description:
+      "Client-server system written in Java for trading of virtual assets within departments of a company. Facilitated trades via a marketplace model where users can buy and sell assets. Admins of an organisation can create assets, users and modify details. Uses MariaDB as a database and Swing for the GUI.",
+    tags: ["Java", "MariaDB", "Swing"],
+    icons: ["logos:java", "logos:mariadb-icon"],
+    imageUrl: assetTradingImg,
+    githubLink: "https://github.com/ben04rogers/organisation-asset-trading",
+  },
+  {
+    title: "Computer Auction",
+    description:
+      "Computer auction application built with Flask. The site allows users to register, bid on items, leave reviews, post new listings, manage listings, search by keyword, and keep a watch list. ",
+    tags: ["Python", "Flask", "Bootstrap", "SQLite"],
+    icons: ["logos:python", "logos:bootstrap", "logos:sqlite"],
+    imageUrl: techprowlImg,
+    githubLink: "https://github.com/ben04rogers/computer-auction",
+  },
+  {
+    title: "Task Manager Console App",
+    description:
+      "Console app that manages tasks in a project. Users can load projects from a file and generate a seqeuence to complete them in, based on each task's dependencies. Users can also find earliest possible commencement time of each task, add new tasks, update tasks, remove tasks, and save the results to a text file.",
+    tags: ["C#"],
+    icons: ["devicon:csharp"],
+    imageUrl: taskManagerImg,
+    githubLink: "https://github.com/ben04rogers/task-manager",
+  },
+  // {
+  //   title: "Family Tree Relationship Path Finder",
+  //   description:
+  //     "Python program that computes the shortest paths in a family tree using a breadth-first search algorithm. Problem was to calculate the shortest path from a starting vertex in a graph to each other vertex. The vertices represent people and each person is related to every other person through parent-child relationships. A person can see how they are related to each other person in the tree",
+  //   tags: ["Python"],
+  //   icons: ["logos:python"],
+  //   imageUrl: familyTreeImg,
+  //   githubLink: "https://github.com/ben04rogers/breadth-first-search",
+  //   demoLink: "https://www.youtube.com/watch?v=VXCZKsqupxA",
+  // },
+  {
+    title: "Arduino Binary Game",
+    description:
+      "Binary game written in C that runs on an Arduino Uno using Tinkercad. The game aims to help users learn binary by challenging them to input different integers in their binary form within a time limit.",
+    tags: ["C"],
+    icons: ["devicon:c"],
+    imageUrl: binaryGameImg,
+    githubLink: "https://github.com/ben04rogers/binary-game-microcontroller",
+    demoLink: "https://www.youtube.com/watch?v=A6n6XDk4Unw&feature=youtu.be",
+  },
+] as const;
+
+export const skillsData = [
+  {
+    category: "Backend",
+    skills: [
+      { name: "TypeScript", icon: "logos:typescript-icon" },
+      { name: "PHP", icon: "logos:php" },
+      { name: "C#", icon: "logos:c-sharp" },
+      { name: "Python", icon: "logos:python" },
+      { name: "Laravel", icon: "logos:laravel" },
+      { name: "Node.js", icon: "logos:nodejs-icon" },
+      { name: "SQL" },
+      { name: "PostgreSQL", icon: "logos:postgresql" },
+      { name: "MySQL", icon: "logos:mysql" },
+      { name: "DynamoDB", icon: "logos:aws-dynamodb" },
+      { name: "REST APIs", icon: "material-symbols:api-outline" },
+      { name: "GraphQL", icon: "logos:graphql" },
+      { name: "Elasticsearch", icon: "logos:elasticsearch" },
+      { name: "Logstash", icon: "logos:logstash" },
+    ],
+  },
+  {
+    category: "Frontend",
+    skills: [
+      { name: "React.js", icon: "logos:react" },
+      { name: "Next.js", icon: "logos:nextjs-icon" },
+      { name: "Redux", icon: "logos:redux" },
+      { name: "HTML5", icon: "logos:html-5" },
+      { name: "CSS3", icon: "logos:css-3" },
+      { name: "Tailwind CSS", icon: "logos:tailwindcss-icon" },
+      { name: "Webpack", icon: "logos:webpack" },
+      { name: "Single Page Applications" },
+      { name: "Responsive Design" }
+    ],
+  },
+  {
+    category: "DevOps",
+    skills: [
+      { name: "AWS", icon: "logos:aws" },
+      { name: "Docker", icon: "logos:docker-icon" },
+      { name: "Kubernetes", icon: "logos:kubernetes" },
+      { name: "Jenkins", icon: "logos:jenkins" },
+      { name: "GitHub Actions", icon: "logos:github-actions" },
+      { name: "Linux", icon: "logos:linux-tux" },
+      { name: "CI/CD" },
+      { name: "Infrastructure as Code" },
+    ],
+  },
+  {
+    category: "Practices",
+    skills: [
+      { name: "Database design" },
+      { name: "Event‑driven architecture" },
+      { name: "Agile" },
+      { name: "Scrum" },
+      { name: "Object Oriented Programming (OOP)" },
+      { name: "Test Driven Development (TDD)" },
+    ],
+  },
+  {
+    category: "Tools",
+    skills: [
+      { name: "Git", icon: "logos:git-icon" },
+      { name: "GitHub", icon: "logos:github-icon" },
+      { name: "Sentry", icon: "logos:sentry-icon" },
+      { name: "New Relic", icon: "logos:new-relic-icon" },
+      { name: "Grafana", icon: "logos:grafana" },
+    ],
+  },
+];
